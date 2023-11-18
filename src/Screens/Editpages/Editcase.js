b89088907e4f3aa4b7ba7b5d64efb2f6144fe410
Rahil Azhar <rahil.azhar10.com>
2023-11-19 00:00:49 +0500

    first commit

--- src/Screens/Editpages/Editcase.js
+++ src/Screens/Editpages/Editcase.js
@@ -22,24 +22,24 @@ const Editcase = () => {
     const [responseMessage, setResponseMessage] = useState('');
 
 
-     const user = sessionStorage.getItem('user')
-     const token = user? JSON.parse(user).token : null
+    const user = sessionStorage.getItem('user')
+    const token = user ? JSON.parse(user).token : null
     const Updatecasehandler = async (e) => {
         e.preventDefault();
-    
+
         const updateddata = {
             title: title
         };
-    
+
         try {
-            let response = await axios.put(`${urlapi}/api/v1/auth/editentries/${id}`, updateddata , {
+            let response = await axios.put(`${urlapi}/api/v1/auth/editentries/${id}`, updateddata, {
                 headers: {
                     'Authorization': `Bearer ${token}` // Include the token in the header
                 }
             });
             // Update the message extraction here
             setResponseMessage(response.data.message || "Entry updated successfully");
-            
+
         } catch (error) {
             console.error('Error updating data:', error);
             // Update this part to handle errors more gracefully
@@ -52,7 +52,7 @@ const Editcase = () => {
             }
         }
     };
-    
+
 
 
 
@@ -92,18 +92,18 @@ const Editcase = () => {
     }
 
 
-    const Adminrequest = async() => {
+    const Adminrequest = async () => {
         try {
-            
-            const response = await fetch(`http://localhost:8082/api/v1/auth/reqedit/${id}`, {
+
+            const response = await fetch(`${urlapi}/api/v1/auth/reqedit/${id}`, {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
             });
-    
+
             const data = await response.json();
-    
+
             if (response.status === 200) {
                 alert('Edit request submitted successfully');
             } else {
@@ -122,59 +122,59 @@ const Editcase = () => {
             <body className='bg-gray-100 min-h-screen w-full flex items-center justify-center'>
                 <div className='w-[1000px] mx-auto bg-white p-6 rounded-lg shadow-lg'>
                     <h2 class="text-2xl font-semibold mb-4 text-center">Edit Case</h2>
-                   
-
-
-                        <div className=' flex  justify-evenly mt-3'>
-                            <TextField className=' w-1/3' id="standard-basic" label="Title" variant="standard"
-                                onChange={titlehandler} value={title} />
 
 
 
-                            <TextField className=' w-1/3' id="standard-basic" label="Suit No" variant="standard"
-                                onChange={(e) => setSuitNo(e.target.value)} value={Suitno} />
-                        </div>
+                    <div className=' flex  justify-evenly mt-3'>
+                        <TextField className=' w-1/3' id="standard-basic" label="Title" variant="standard"
+                            onChange={titlehandler} value={title} />
 
 
-                        <div className=' flex  justify-evenly mt-3'>
-                            <TextField className=' w-1/3' id="standard-basic" label="Nature" variant="standard"
-                                onChange={(e) => setNature(e.target.value)} value={nature} multiline />
-                            <TextField className=' w-1/3' id="standard-basic" label="Previous hearing" variant="standard"
-                                onChange={(e) => setPrevHearing(e.target.value)} value={prevhearing} />
-                        </div>
-                        <div className=' flex  justify-evenly mt-3'>
-                            <TextField className=' w-1/3' id="standard-basic" label="Next hearing" variant="standard"
-                                onChange={(e) => setNextHearing(e.target.value)} value={nexthearing} />
-                            <TextField className=' w-1/3' id="standard-basic" label="Factsheet" variant="standard"
-                                onChange={(e) => setFactSheet(e.target.value)} value={factsheet} />
-                        </div>
-                        <div className=' flex justify-start ml-[100px]'>
-                            <FormControl variant="standard" sx={{ m: 1, minWidth: 190 }}>
-                                <InputLabel className=' w-full' id="demo-simple-select-standard-label">Progress Report</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-standard-label"
-                                    id="demo-simple-select-standard"
-                                    value={progressreport}
-                                    label="Age"
-                                    onChange={(e) => setProgressReport(e.target.value)}
-                                >
 
-                                    <MenuItem value="Yes">Yes</MenuItem>
-                                    <MenuItem value="No">No</MenuItem>
+                        <TextField className=' w-1/3' id="standard-basic" label="Suit No" variant="standard"
+                            onChange={(e) => setSuitNo(e.target.value)} value={Suitno} />
+                    </div>
 
-                                </Select>
-                            </FormControl>
-                        </div>
-                        <div className=' flex justify-center mt-10'>
-                            <button className=' bg-purple-300 px-10 py-2 rounded hover:text-white' type='button' onClick={Updatecasehandler}>Update</button>
-                           
-                        </div>
-                        <div className=' flex justify-center mt-10'>
-                            <button className=' bg-amber-300 px-10 py-2 rounded hover:text-white' type='button' onClick={Adminrequest}>Request For Admin to Allow you to Edit</button>
-                           
-                        </div>
-                     <div className='flex justify-center mt-5 text-lg '>   {responseMessage && responseMessage}</div>
-                    
+
+                    <div className=' flex  justify-evenly mt-3'>
+                        <TextField className=' w-1/3' id="standard-basic" label="Nature" variant="standard"
+                            onChange={(e) => setNature(e.target.value)} value={nature} multiline />
+                        <TextField className=' w-1/3' id="standard-basic" label="Previous hearing" variant="standard"
+                            onChange={(e) => setPrevHearing(e.target.value)} value={prevhearing} />
+                    </div>
+                    <div className=' flex  justify-evenly mt-3'>
+                        <TextField className=' w-1/3' id="standard-basic" label="Next hearing" variant="standard"
+                            onChange={(e) => setNextHearing(e.target.value)} value={nexthearing} />
+                        <TextField className=' w-1/3' id="standard-basic" label="Factsheet" variant="standard"
+                            onChange={(e) => setFactSheet(e.target.value)} value={factsheet} />
+                    </div>
+                    <div className=' flex justify-start ml-[100px]'>
+                        <FormControl variant="standard" sx={{ m: 1, minWidth: 190 }}>
+                            <InputLabel className=' w-full' id="demo-simple-select-standard-label">Progress Report</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-standard-label"
+                                id="demo-simple-select-standard"
+                                value={progressreport}
+                                label="Age"
+                                onChange={(e) => setProgressReport(e.target.value)}
+                            >
+
+                                <MenuItem value="Yes">Yes</MenuItem>
+                                <MenuItem value="No">No</MenuItem>
+
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className=' flex justify-center mt-10'>
+                        <button className=' bg-purple-300 px-10 py-2 rounded hover:text-white' type='button' onClick={Updatecasehandler}>Update</button>
+
+                    </div>
+                    <div className=' flex justify-center mt-10'>
+                        <button className=' bg-amber-300 px-10 py-2 rounded hover:text-white' type='button' onClick={Adminrequest}>Request For Admin to Allow you to Edit</button>
+
+                    </div>
+                    <div className='flex justify-center mt-5 text-lg '>   {responseMessage && responseMessage}</div>
+
                 </div>
 
             </body>
